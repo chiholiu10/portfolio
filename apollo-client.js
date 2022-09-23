d43331b78f32bb22f05ff925d00299e47605241f
chiho <chiholiu10@gmail.com>
2022-09-23 20:12:26 +0200

    uri changed

--- apollo-client.js
+++ apollo-client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const API_URL = process.env.STRAPI_PRODUCTION || "http://localhost:1337";
 
 const client = new ApolloClient({
-  uri: `http://localhost:1337/graphql`,
+  uri: `${API_URL}/graphql`,
   cache: new InMemoryCache(),
 });
 
