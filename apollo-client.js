2b9727f7a6a81d56621b83408974592237b7f623
chiho <chiholiu10@gmail.com>
2022-09-25 17:32:02 +0200

    change url

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const API_URL = process.env.NODE_ENV === 'production' ? process.env.STRAPI_PRODUCTION : "http://localhost:1337";
-console.log(API_URL, process.env.STRAPI_PRODUCTION);
+const API_URL = process.env.NODE_ENV !== 'production' ? process.env.NEXT_STRAPI_PRODUCTION : "http://localhost:1337";
 
 const client = new ApolloClient({
   uri: `${API_URL}/graphql`,
