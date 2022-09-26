a0446ee9f37cdc3d3775d288e17a66d0a978b2a3
chiho <chiholiu10@gmail.com>
2022-09-27 01:22:23 +0200

    env variables added

--- next.config.js
+++ next.config.js
@@ -5,8 +5,11 @@ const nextConfig = {
     domains: ["localhost"],
   },
   env: {
+    NODE_ENV: process.env.NODE_ENV,
     NEXT_API_URL: process.env.NEXT_API_URL,
-    NEXT_STRAPI_PRODUCTION: process.env.NEXT_STRAPI_PRODUCTION
+    NEXT_STRAPI_PRODUCTION: process.env.NEXT_STRAPI_PRODUCTION,
+    NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
+    NEXT_PUBLIC_KEY: process.env.NEXT_PUBLIC_KEY
   },
   trailingSlash: true,
 };
