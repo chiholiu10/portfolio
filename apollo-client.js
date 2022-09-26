efac060666c4608ffcb03ed28e3a59bcb45f5d7a
chiho <chiholiu10@gmail.com>
2022-09-27 01:25:48 +0200

    update env

--- apollo-client.js
+++ apollo-client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const API_URL = process.env.NODE_ENV === 'production' ? process.env.NEXT_API_URL : "http://localhost:1337";
+const API_URL = process.env.NEXT_NODE_ENV === 'production' ? process.env.NEXT_API_URL : "http://localhost:1337";
 
 const client = new ApolloClient({
   uri: `${API_URL}/graphql`,
