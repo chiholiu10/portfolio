123acdfcd625dcf4925e5df3570eea6a3f794346
chiho <chiholiu10@gmail.com>
2022-10-22 23:35:09 +0200

    added changes

--- apollo-client.js
+++ apollo-client.js
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const API_URL = (process && process.env.NODE_ENV === 'production') ? "https://portfolioserverside.herokuapp.com" : "http://localhost:1337";
+const API_URL = (process && process.env.NODE_ENV === 'production') ? "https://portfolioserverside.herokuapp.com" : "http://localhost:1337/graphql";
 const client = new ApolloClient({
-  uri: `${API_URL}/graphql`,
+  uri: `${API_URL}`,
   cache: new InMemoryCache(),
 });
 
