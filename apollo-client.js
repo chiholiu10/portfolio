bf912c344bd5e3a2ef75111761345c5656ec2e02
chiho <chiholiu10@gmail.com>
2022-10-22 19:54:02 +0200

    production check

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const API_URL = (process && process.env.NODE_ENV === 'development') ? "http://localhost:1337/graphql" : 'https://portfolioserverside.herokuapp.com/graphql';
-console.log(process.env.NODE_ENV, process);
+console.log(process.env.NODE_ENV === 'development');
+console.log(process.env.NODE_ENV === 'production');
 const client = new ApolloClient({
   uri: `${API_URL}`,
   cache: new InMemoryCache(),
