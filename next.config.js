75b9aed46b2bbb0c1646c87d1aa1d46d82688759
chiho <chiholiu10@gmail.com>
2022-10-24 00:39:38 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-  //   domains: ["localhost"],
-  // },
-
+  images: {
+    domains: ["localhost"],
+  },
+  experimental: {
+    forceSwcTransforms: true,
+  },
   env: {
-    NEXT_NODE_ENV: "production",
-    NEXT_API_URL: "https://portfolioserverside.herokuapp.com",
+    NEXT_NODE_ENV: process.env.NEXT_NODE_ENV,
+    NEXT_API_URL: process.env.NEXT_API_URL,
     NEXT_STRAPI_PRODUCTION: process.env.NEXT_STRAPI_PRODUCTION,
     NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
     NEXT_PUBLIC_KEY: process.env.NEXT_PUBLIC_KEY
