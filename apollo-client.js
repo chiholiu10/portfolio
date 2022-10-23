ded0c7df3ed26ae0dc1dba6ebca2f91213ff51ed
chiho <chiholiu10@gmail.com>
2022-10-23 04:56:44 +0200

    update issue

--- apollo-client.js
+++ apollo-client.js
@@ -1,25 +1,9 @@
-import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
-import { onError } from "@apollo/client/link/error";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const API_URL = (process && process.env.NODE_ENV === 'production') ? "https://portfolioserverside.herokuapp.com" : "http://localhost:1337";
 
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-      ),
-    );
-
-  if (networkError) console.log(`[Network error]: ${networkError}`);
-});
-
-const httpLink = new HttpLink({
-  uri: `${API_URL}/graphql`
-});
-
 const client = new ApolloClient({
-  link: from([httpLink, errorLink]),
+  uri: `${API_URL}/graphql`,
   cache: new InMemoryCache(),
 });
 
