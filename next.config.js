ed701205fd6c299831f9b2a1b8ca90fe4680d4fa
Chiho <chiho@Chihos-MacBook-Pro.local>
2022-11-26 01:12:45 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_SPACE: process.env.NEXT_SPACE
+    NEXT_SPACE: process.env.NEXT_SPACE,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        port: "",
+      },
+    ],
   },
 };
 
