43e4ed22cd202819e8afdc411da5e3a0a10ac0b2
Chiho <chiho@Chihos-MacBook-Pro.local>
2023-04-09 13:34:09 +0200

    update portfolio with rampage

--- components/Sections/Portfolio/Portfolio.styles.ts
+++ components/Sections/Portfolio/Portfolio.styles.ts
@@ -1,9 +1,13 @@
 import styled from "styled-components";
+import { breakpoint } from "styles/Breakpoint";
 
 export const PortfolioImage = styled.img`
-  max-width: 400px;
+  max-width: 300px;
   width: 100%;
   display: flex;
   flex-direction: row;
   margin: 10px;
+  ${breakpoint.md`
+    max-width: 400px;
+  `}
 `;
