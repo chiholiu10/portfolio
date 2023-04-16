262ddd0f8ea6a40dad7ab6ab7ff69008ec0a7c97
Chiho <chiho@chihos-mbp.home>
2023-04-16 18:31:46 +0200

    update

--- components/Sections/Banner/Banner.styles.ts
+++ components/Sections/Banner/Banner.styles.ts
@@ -4,3 +4,30 @@ import { HeaderGeneral } from "styles/General.styles";
 export const HeaderH1 = styled.h1`
   ${HeaderGeneral}
 `;
+
+export const ProfileCartoon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  img {
+    z-index: -1;
+    opacity: 0.3;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    top: 0;
+    width: 900px;
+    height: 400px;
+  }
+  svg {
+    position: relative;
+    opacity: 1;
+    z-index: 100;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
--- components/Sections/Introduction/Introduction.styles.ts
+++ components/Sections/Introduction/Introduction.styles.ts
@@ -3,14 +3,15 @@ import { TilesProps, TitleBlockProps } from "styles/General.styles";
 import { breakpoint } from "../../../styles/Breakpoint";
 
 export const IntroBlock = styled.div`
-  width: 120px;
-  height: 120px;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
+  text-align: left;
+  padding: 20px;
   display: flex;
   overflow-x: hidden;
   ${TilesProps}
   ${breakpoint.sm`
+    justify-content: center;
+    align-items: center;
     width: 150px;
     height: 150px;
   `}
@@ -22,11 +23,19 @@ export const IntroSubTitle = styled.p`
   font-weight: 600;
 `;
 
-export const IntroBlockCenter = styled.div`
-  text-align: center;
-  z-index: 10;
-`;
-
 export const IntroTitle = styled.h3`
   ${TitleBlockProps}
+`;
+
+export const IntroBlockCenter = styled.div`
+  z-index: 10;
+  display: flex;
+  flex-direction: row;
+  ${IntroTitle} {
+    min-width: 130px;
+  }
+  ${breakpoint.sm`
+    text-align: center;
+    flex-direction: column;
+  `}
 `;
