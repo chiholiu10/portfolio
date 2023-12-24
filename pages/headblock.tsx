016e3195a52b73a7d5430ed04c3a5e3560eecf8b
chiho <chiho.liu@vodafone.com>
2023-12-24 14:17:08 +0100

    update

--- pages/headblock.tsx
+++ pages/headblock.tsx
@@ -1,7 +1,7 @@
 const HeadBlock = () => (
   <>
     <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta http-equiv="Content-Security-Policy" content="script-src 'none'"/>
+    <meta httpEquiv="Content-Security-Policy" content="script-src 'none'"/>
     <title>Portfolio Chi Ho Liu | Front End Developer</title>
     <link rel="icon" href="/favicon.ico" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
