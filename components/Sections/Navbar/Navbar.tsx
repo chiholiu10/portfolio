5d60e38a083b8e1a63c1f361e10fda191424eaa6
chiho <chiholiu10@gmail.com>
2024-12-08 20:07:59 +0100

    Revert "fix: underline animation scroll navbar"
    This reverts commit 7489761052f846f8ea454635de807ee174632176.

--- components/Sections/Navbar/Navbar.tsx
+++ components/Sections/Navbar/Navbar.tsx
@@ -12,44 +12,37 @@ export const Navbar = () => {
   const [navEffect, setNavEffect] = useState<boolean>(true);
   const [isLoading, setIsLoading] = useState(true);
 
-  const handleLoading = () => {
-    setIsLoading(false);
-  };
-
   useEffect(() => {
     const handleScroll = () => {
       const yValue = global?.window.scrollY;
       yValue < 5 ? setNavEffect(true) : setNavEffect(false);
     };
-    window.addEventListener("load", handleLoading);
-    document.addEventListener("scroll", handleScroll, false);
-    handleScroll();
 
-    console.log(isLoading);
+    handleScroll();
     
     return () => {
       document.removeEventListener("scroll", handleScroll, false);
-      window.removeEventListener("load", handleLoading);
     };
   }, [isLoading]);
 
-  return (
-    <NavbarComponent naveffect={navEffect}>
-      <NavbarInnerComponent>
-        <NavbarInnerBlock>
-          <FadeRightToLeft>
-          <span>Chiho Liu</span>
-          </FadeRightToLeft>
-        </NavbarInnerBlock>
-        <Logo>
-          <Image
-            src="https://res.cloudinary.com/dh7tnzzxm/image/upload/v1681677951/Frame_1_1_mh4oma.png"
-            alt="chiho-website-icon"
-            width={100}
-            height={100}
-          />
-        </Logo>
-      </NavbarInnerComponent>
-    </NavbarComponent>
+    return (isLoading ? (
+      <NavbarComponent naveffect={navEffect}>
+        <NavbarInnerComponent>
+          <NavbarInnerBlock>
+            <FadeRightToLeft>
+            <span>Chiho Liu</span>
+            </FadeRightToLeft>
+          </NavbarInnerBlock>
+          <Logo>
+            <Image
+              src="https://res.cloudinary.com/dh7tnzzxm/image/upload/v1681677951/Frame_1_1_mh4oma.png"
+              alt="chiho-website-icon"
+              width={100}
+              height={100}
+            />
+          </Logo>
+        </NavbarInnerComponent>
+      </NavbarComponent>
+    ) : ( <NavbarComponent navEffect={navEffect}></NavbarComponent>)
   )
 };
