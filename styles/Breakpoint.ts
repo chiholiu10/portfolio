f16d47665cf7754ff125b7f516cde5ce2a5f5b8e
chiho <chiholiu10@gmail.com>
2025-02-04 00:14:05 +0100

    fix: update with design system

--- styles/Breakpoint.ts
+++ styles/Breakpoint.ts
@@ -1,14 +1,13 @@
-import { css } from "styled-components";
 import theme from "./Theme";
 
-export const breakpoint: Record<string, (strings: TemplateStringsArray, ...args: string[]) => ReturnType<typeof css>> = Object.keys(theme.breakpoints).reduce(
-  (accumulator, label) => {
-    accumulator[label] = (strings, ...args) => css`
-      @media (min-width: ${theme.breakpoints[label]}) {
-        ${css(strings, ...args)};
-      }
-    `;
-    return accumulator;
+export const breakpoint = Object.keys(theme.breakpoints).reduce(
+  (acc, label) => {
+    acc[label] = (styles) => `
+    @media (min-width: ${theme.breakpoints[label]}) {
+      ${styles}
+    }
+  `;
+    return acc;
   },
-  {} as Record<string, (strings: TemplateStringsArray, ...args: string[]) => ReturnType<typeof css>>
+  {}
 );
---
+++ styles/token.ts
@@ -0,0 +1,38 @@
+import { DefaultTheme } from "styled-components";
+
+const token: DefaultTheme = {
+  // colors
+  colors: {
+    white: "#FFFFFF",
+    darkBlack: "#000000",
+    black: "#000000eb",
+    lightBlack: "#2c2c2c",
+    ultraLightBlack: "#2c2c2cf2",
+    grey: "#999999",
+    blue: "#00b6ff;",
+    transparent: "transparent",
+  },
+  typoGraphy: {
+    fontWeights: {
+      regular: 300,
+      normal: 400,
+      semiBold: 600,
+      bold: 700,
+      extraBold: 800,
+      black: 900,
+    },
+    fonts: {
+      heading: '"TWKEverett", sans-serif',
+      body: '"TWKEverett", sans-serif',
+    },
+  },
+  breakpoints: {
+    xs: "360px",
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1440px",
+  },
+};
+
+export default token;
