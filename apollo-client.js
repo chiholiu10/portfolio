a31d828b4b20754af9bbfafce4bbec7fd3b946b6
chiho <chiholiu10@gmail.com>
2025-02-10 10:26:02 +0100

    fix: updated graphql security

--- apollo-client.js
+++ apollo-client.js
@@ -1,4 +1,9 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  ApolloLink,
+  InMemoryCache,
+} from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const contentToken = "Efc8fVC1vz-dSug8XJUneKcS-LXCpE_pNtr0a6_JfFA";
@@ -14,8 +19,27 @@ const authLink = setContext((_, { headers }) => ({
   },
 }));
 
+const introspectionBlockLink = new ApolloLink((operation, forward) => {
+  if (
+    process.env.NODE_ENV === "production"
+    && operation.query.definitions.some(
+      (def) => def.kind === "OperationDefinition"
+        && def.selectionSet.selections.some(
+          (selection) => selection.name?.value === "__schema"
+            || selection.name?.value === "__type",
+        ),
+    )
+  ) {
+    console.warn("Introspection is disabled in production.");
+    return null; // Block the request
+  }
+  return forward(operation);
+});
+
+console.log(introspectionBlockLink);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: ApolloLink.from([authLink, introspectionBlockLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
